993c97ab9a385eac4ffe2137de429809782285da
nilesh44 <nileshkumarjaiswal7@gmail.com>
2022-07-14 12:39:01 +0530

    remove comment

--- src/main/java/com/ace/customer/controller/CustomerController.java
+++ src/main/java/com/ace/customer/controller/CustomerController.java
@@ -34,23 +34,5 @@ public class CustomerController {
 		
 		return ResponseEntity.ok(customerService.getCustomerByMobile(customerByMobileRequest.getMobileNum()));
 	}
-	/*
-	 * @PostMapping("/getCustomerByMobile") public
-	 * ResponseEntity<GetCustomerByMobileResponse> getCustomerByMobile(@RequestBody
-	 * GetCustomerByMobileRequest customerByMobileRequest){
-	 * 
-	 * return
-	 * ResponseEntity.ok(customerService.getCustomerByMobile(customerByMobileRequest
-	 * .getMobileNum())); }
-	 */
 	
-	/*
-	 * @PostMapping("/getCustomerByMobile") public
-	 * ResponseEntity<GetCustomerByMobileResponse> getCustomerByMobile(@RequestBody
-	 * GetCustomerByMobileRequest customerByMobileRequest){
-	 * 
-	 * return
-	 * ResponseEntity.ok(customerService.getCustomerByMobile(customerByMobileRequest
-	 * .getMobileNum())); }
-	 */
 }
